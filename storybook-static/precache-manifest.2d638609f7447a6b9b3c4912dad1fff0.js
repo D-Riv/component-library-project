dc6d444fb16cac11203072100a93acf75a78a78c
Dennis Rivera <rivd19@outlook.com>
2020-02-21 16:11:58 -0500

    Test deployment 2

---
+++ storybook-static/precache-manifest.2d638609f7447a6b9b3c4912dad1fff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9328e830ea143ea228b796283ceb488",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a61f67a121cdce0ca882.bundle.js"
+  },
+  {
+    "url": "runtime~main.a61f67a121cdce0ca882.bundle.js"
+  },
+  {
+    "revision": "1a32934d303b057db3ee",
+    "url": "static/css/main.8e250082.chunk.css"
+  },
+  {
+    "url": "vendors~main.a61f67a121cdce0ca882.bundle.js"
+  },
+  {
+    "url": "vendors~main.a61f67a121cdce0ca882.bundle.js.LICENSE.txt"
+  }
+]);
